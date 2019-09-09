390f62069ced106fae9f6d9b7f65b9ca803188ac
Piyush7034 <piyushshukla2100@gmail.com>
2019-09-09 16:34:20 +0530

    few ui-changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: LoginPage(),
+        ),
       ),
       title: 'Messenger App',
     );
