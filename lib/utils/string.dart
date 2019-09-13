1e1ae2c6b6a9ab4ee613047d667c20f0e8aa8829
Piyush7034 <piyushshukla2100@gmail.com>
2019-09-13 15:46:29 +0530

    added string and styles file

---
+++ lib/utils/string.dart
@@ -0,0 +1,8 @@
+const MY_CHAT_UP = "My Chat App";
+const LOG_IN_BUTTON = "Login";
+const SIGN_UP_BUTTON = "Sign Up";
+const ADD_USER = "Add User";
+const CAMERA = "Camera";
+const CHATS = "Chats";
+const STATUS = "Status";
+const CONTACTS = "Contacts";
