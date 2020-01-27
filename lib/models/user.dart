ca52ef3ea0b6066d0b53c68384dfb326b8d9733b
piyush_shukla_79 <piyushshukla2100@gmail.com>
2020-01-27 17:03:30 +0530

    incomplete

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+import 'package:mobx/mobx.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
+class User extends _User with _$User {
+  static User fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  static Map<String, dynamic> toJson(User user) => _$UserToJson(user);
+}
+
+abstract class _User with Store {}
